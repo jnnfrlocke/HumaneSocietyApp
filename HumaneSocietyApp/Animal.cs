7cdb66d0517a3dfb6999bb2063b67bf4f5d3e82c
Jen <jnnfrlocke@gmail.com>
2017-10-06 14:27:11 -0500

    Create individual methods for getting animal info

--- HumaneSocietyApp/Animal.cs
+++ HumaneSocietyApp/Animal.cs
@@ -6,47 +6,29 @@ using System.Threading.Tasks;
 
 namespace HumaneSocietyApp
 {
-    class Animal
+    public class Animal
     {
+        string addName;
+        string speciesToAdd;
+        string vaccinationAnswer;
+        double foodPerWeek;
+        string locationToAdd;
+        string adoptionStatus;
+        int adoptionFee;
+        string specialNeeds;
+        int age;
+
         public void Add()
         {
-            List<string> animalToAdd = new List<string>();
-
-            Console.WriteLine("What's the animal's name?");
-            string addName = Console.ReadLine();
-            animalToAdd.Add(addName);
-
-            Console.WriteLine("Please type the animal's species.");
-            string speciesToAdd = Console.ReadLine();
-            animalToAdd.Add(speciesToAdd);
-
-            Console.WriteLine("Is this animal vaccinated?");
-            string vaccinationAnswer = Console.ReadLine();
-            animalToAdd.Add(vaccinationAnswer);
-
-            Console.WriteLine("How much food does this animal eat per week, in cups?");
-            double foodPerWeek = double.Parse(Console.ReadLine());
-            //animalToAdd.Add(foodPerWeek);
-
-            Console.WriteLine("What room is this animal in?");
-            string locationToAdd = Console.ReadLine();
-            animalToAdd.Add(locationToAdd);
-
-            Console.WriteLine("Has this animal been adopted?");
-            string adoptionStatus = Console.ReadLine();
-            animalToAdd.Add(adoptionStatus);
-
-            Console.WriteLine("What is this animal's adoption fee?");
-            int adoptionFee = int.Parse(Console.ReadLine());
-            //animalToAdd.Add(adoptionFee);
-
-            Console.WriteLine("What kind of special needs does this animal have?");
-            string specialNeeds = Console.ReadLine();
-            animalToAdd.Add(specialNeeds);
-
-            Console.WriteLine("How old is this animal?");
-            int age = int.Parse(Console.ReadLine());
-            //animalToAdd.Add(age);
+            addName = GetName();
+            speciesToAdd = GetSpecies();
+            vaccinationAnswer = GetVaccination();
+            foodPerWeek = GetFood();
+            locationToAdd = GetLocation();
+            adoptionStatus = GetAdoptionStatus();
+            adoptionFee = GetAdoptionFee();
+            specialNeeds = GetSpecialNeeds();
+            age = GetAge();
 
             animal animalToInsert = new animal
             {
@@ -66,6 +48,74 @@ namespace HumaneSocietyApp
             addAnimal.animals.InsertOnSubmit(animalToInsert);
             addAnimal.SubmitChanges();
         }
+
+        private string GetName()
+        {
+            Console.WriteLine("What's the animal's name?");
+            string addName = Console.ReadLine();
+            return addName;
+        }
+
+        private string GetSpecies()
+        {
+            Console.WriteLine("Please type the animal's species.");
+            string speciesToAdd = Console.ReadLine();
+            return speciesToAdd;
+        }
+
+        private string GetVaccination()
+        {
+            Console.WriteLine("Is this animal vaccinated?");
+            string vaccinationAnswer = Console.ReadLine();
+            return vaccinationAnswer;
+        }
+
+        private double GetFood()
+        {
+            Console.WriteLine("How much food does this animal eat per week, in cups?");
+            double foodPerWeek = double.Parse(Console.ReadLine());
+            return foodPerWeek;
+        }
+
+        private string GetLocation()
+        {
+
+            Console.WriteLine("What room is this animal in?");
+            string locationToAdd = Console.ReadLine();
+            return locationToAdd;
+        }
+
+        private string GetAdoptionStatus()
+        {
+            Console.WriteLine("Has this animal been adopted?");
+            string adoptionStatus = Console.ReadLine();
+            return adoptionStatus;
+        }
+
+        private int GetAdoptionFee()
+        {
+            Console.WriteLine("What is this animal's adoption fee?");
+            int adoptionFee = int.Parse(Console.ReadLine());
+            return adoptionFee;
+        }
+
+        private string GetSpecialNeeds()
+        {
+            Console.WriteLine("What kind of special needs does this animal have?");
+            string specialNeeds = Console.ReadLine();
+            return specialNeeds;
+        }
+
+        private int GetAge()
+        {
+            Console.WriteLine("How old is this animal?");
+            int age = int.Parse(Console.ReadLine());
+            return age;
+        }
+
+
+
+
 
         public void ChangeStatus()
         {
