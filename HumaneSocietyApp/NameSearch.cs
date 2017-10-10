ddaa16b7ca89f02b3a1206ea28553e35c5526448
Jen <jnnfrlocke@gmail.com>
2017-10-10 09:53:39 -0500

    NameSearch complete

--- HumaneSocietyApp/NameSearch.cs
+++ HumaneSocietyApp/NameSearch.cs
@@ -18,7 +18,7 @@ namespace HumaneSocietyApp
             IQueryable<animal> namesQuery =
                 from animal in db.animals
                 where animal.name == searchName
-                select animal;// TODO: capture this in an array/list to use in a narrow by search
+                select animal;
 
             Array namesArray = namesQuery.ToArray();
 
@@ -26,7 +26,7 @@ namespace HumaneSocietyApp
             {
                 foreach (var result in namesQuery)
                 {
-                    Console.WriteLine($"Located {searchName}, ID :{result.animal_id}, {result.species}, aged {result.age}");
+                    Console.WriteLine($"Located {searchName}, ID:{result.animal_id}, {result.species}, aged {result.age}");
                 }
             }
             catch (InvalidCastException)
@@ -36,8 +36,6 @@ namespace HumaneSocietyApp
 
             NarrowSearch narrowSearchDown = new NarrowSearch();
             narrowSearchDown.narrowOption(namesArray);
-
-            //return searchName;
         }
     }
 }
--- HumaneSocietyApp/NarrowSearch.cs
+++ HumaneSocietyApp/NarrowSearch.cs
@@ -20,14 +20,37 @@ namespace HumaneSocietyApp
             }
             else
             {
-                Console.WriteLine("Would you like to exit the application or start over? Type 1 to exit or 2 to start over.");
+                Console.WriteLine("Would you like to perform an action on an animal's record , exit the application or start over? Type 1 to edit an animal's record, 2 to exit, or 3 to start over.");
                 string endOrRestart = Console.ReadLine().ToLower();
 
                 if (endOrRestart == "1")
                 {
+                    Console.WriteLine("To edit an animal's species type 1, to change an animal's adoption status type 2, or to process an adoption fee type 3.");
+                    string actionToPerform = Console.ReadLine();
+
+                    switch (actionToPerform)
+                    {
+                        case "1":
+                            //change species method;
+                            break;
+                        case "2":
+                            //change adoption status method;
+                            break;
+                        case "3":
+                            //process payment method;
+                            break;
+                        default:
+                            Console.WriteLine("You entered an invalid option.");
+                            narrowOption(arrayToNarrow);
+                            break;
+                    }
+                }
+
+                else if (endOrRestart == "2")
+                {
                     Environment.Exit(0);
                 }
-                else if (endOrRestart == "2")
+                else if (endOrRestart == "3")
                 {
                     HumaneSociety startOver = new HumaneSociety();
                     startOver.Run();
@@ -39,7 +62,6 @@ namespace HumaneSocietyApp
                     narrowOption(arrayToNarrow);
                 }
             }
-
         }
     }
 }
